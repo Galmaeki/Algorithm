4653d44b79418e28a717fe12104fb9ebc91813dc
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2023-07-26 21:52:43 +0900

    [unrated] Title: 0 떼기, Time: 0.05 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String n_str) {
+        StringBuilder sb = new StringBuilder();
+        return sb.append(Integer.parseInt(n_str)).toString();
+    }
+}
