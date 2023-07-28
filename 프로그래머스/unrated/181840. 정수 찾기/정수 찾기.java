1ee373cc065d1959400c55766fa82a6370390ea5
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2023-07-28 20:46:52 +0900

    [unrated] Title: 정수 찾기, Time: 0.02 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] num_list, int n) {
+        for(int a : num_list) {
+            if(a==n)
+                return 1;
+        }
+        return 0;
+    }
+}
