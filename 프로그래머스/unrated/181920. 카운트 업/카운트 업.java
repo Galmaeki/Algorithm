4da66f810cb5e11fdaf93e3cc54e576406d4a075
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2023-08-03 19:15:39 +0900

    [unrated] Title: 카운트 업, Time: 0.01 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int start, int end) {
+        int[] answer = new int[end-start+1];
+        for(int i = 0;i<answer.length;i++){
+            answer[i]=start;
+            start+=1;
+        }
+        return answer;
+    }
+}
