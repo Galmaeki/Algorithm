390d4f9a26b799286cc487589631d231cd28328b
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2023-08-16 10:35:06 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashSet hs = new HashSet<Integer>();
+        for(int i =0;i<10;i++){
+            hs.add(Integer.parseInt(br.readLine())%42);
+        }
+        System.out.println(hs.size());
+    }
+}
