1f2611b8358b641227f6607211df2b2282f05ba9
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2023-10-17 18:16:33 +0900

    [Bronze V] Title: 아!, Time: 132 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        System.out.println(str2.length()<=str1.length()?"go":"no");
+    }
+}
