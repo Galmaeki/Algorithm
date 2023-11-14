64139cd4ee0c5a18973ec969104930eaabb00a18
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2023-11-14 11:37:33 +0900

    [Silver IV] Title: 수 찾기, Time: 520 ms, Memory: 45408 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,53 +1,32 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class Main {
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<String,Boolean> hm = new HashMap<>();
         int count = Integer.parseInt(br.readLine());
-        int[] arr = new int[count];
         StringTokenizer st1 = new StringTokenizer(br.readLine());
 
         int target = Integer.parseInt(br.readLine());
-        int[] targets = new int[target];
         StringTokenizer st2 = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < count; i++) {
-            arr[i] = Integer.parseInt(st1.nextToken());
+            hm.put(st1.nextToken(),true);
         }
 
         for (int i = 0; i < target; i++) {
-            targets[i] = Integer.parseInt(st2.nextToken());
-        }
-
-        Arrays.sort(arr);
-
-        for (int i = 0; i < target; i++) {
-            solution(arr, targets[i]);
+            sb.append(hm.getOrDefault(st2.nextToken(),false)?"1":"0").append("\n");
         }
 
         sb.setLength(sb.length()-1);
 
         System.out.println(sb);
-    }
-
-    private static void solution(int[] arr, int target) {
-        int L = 0;
-        int R = arr.length - 1;
-        boolean a= false;
-        while(L<=R){
-            int mid = (L+R)/2;
-            if(arr[mid]==target){//찾앗슴!
-                a = true;
-                break;
-            }else if(arr[mid]<target){//타겟이 더 크니까 L을 땡겨옴
-                L = mid+1;
-            }else if(arr[mid]>target){//타겟이 더 작으니까 R을 땡겨옴
-                R = mid-1;
-            }
-        }
-        sb.append(a?"1":"0").append("\n");
     }
 }
