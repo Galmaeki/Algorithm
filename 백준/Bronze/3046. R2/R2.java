eb5d01b6cdb6e36772ae08f82fb9b5734f36cf0b
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2023-12-29 10:01:58 +0900

    [Bronze IV] Title: R2, Time: 124 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println((b*2)-a);
+    }
+}
