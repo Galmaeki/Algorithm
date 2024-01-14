5e7302fc6458d44b2ed7e68f98a821bb7dce275a
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2024-01-15 01:04:46 +0900

    [Bronze V] Title: 웰컴, Time: 124 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
