035c8b9cff16500ec04e2b241d02011b3e201782
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2024-01-25 23:09:48 +0900

    [Bronze II] Title: OX퀴즈, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -1,23 +1,31 @@
-import java.util.*;
-public class Main{
-public static void main(String[] args){
-            Scanner sc = new Scanner(System.in);
-            int player = sc.nextInt();
-            for(int j=0;j<player;j++){
-            String ox=sc.next();
-            sc.nextLine();
-            int anscount = 0,answer=0;
-            for(int i =0;i<ox.length();i++){
-                if(ox.charAt(i)=='X')
-                {
-                    anscount =0;
-                }else{
-                    anscount++;
-                    answer+=anscount;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int count = Integer.parseInt(br.readLine());
+        String arr[] = new String[count];
+        for (int i = 0; i < count; i++) {
+            arr[i]= br.readLine();
+        }
+
+        for (int i = 0; i < count; i++) {
+            int score=1;
+            int result =0;
+            for (int j = 0; j < arr[i].length(); j++) {
+                if(arr[i].charAt(j)=='O'){
+                    result+=score;
+                    score++;
+                } else {
+                    score=1;
                 }
             }
-            System.out.println(answer);
-            }
-            sc.close();
+            System.out.println(result);
         }
+    }
 }
