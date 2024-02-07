ae568a93de845e5bff17d970119af1af45fb796a
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2024-02-07 23:09:18 +0900

    [Bronze V] Title: 이상한 기호, Time: 128 ms, Memory: 14164 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        System.out.println((a+b)*(a-b));
+
+    }
+}
