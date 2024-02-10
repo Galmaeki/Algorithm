7b46ca08f93e2409622e1808571b7f302bd1cb44
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2024-02-10 13:41:44 +0900

    [Bronze V] Title: 콜센터, Time: 120 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.println("|          |");
+    }
+}
