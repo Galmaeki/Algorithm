1263bd78b2c6c8ac623637390a280dc879f7cf24
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2024-02-18 21:16:04 +0900

    [Bronze IV] Title: 녹색거탑, Time: 116 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int i = Integer.parseInt(br.readLine());
+        System.out.println((int)Math.pow(2,i));
+    }
+}
