974bf9f3a47a921710df7056d68d628cda76f6c2
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2024-02-25 22:07:46 +0900

    [Bronze V] Title: 홍익대학교, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int a = Integer.parseInt(br.readLine());
+
+        System.out.println(a-1946);
+    }
+}
