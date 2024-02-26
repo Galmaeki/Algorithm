96c06c44107efa6de859e5f90ccb0463aab23221
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2024-02-26 19:56:34 +0900

    [Bronze V] Title: 오늘 날짜, Time: 284 ms, Memory: 18816 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        DateFormat df = new SimpleDateFormat("YYYY-MM-dd");
+        df.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+        System.out.println(df.format(new Date()));
+    }
+}
