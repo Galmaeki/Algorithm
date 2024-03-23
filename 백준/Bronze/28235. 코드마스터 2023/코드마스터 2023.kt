2407d8de9565ef70a3f5c064fc95e1f13910c18a
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2024-03-23 20:02:58 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 88 ms, Memory: 12296 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var a = readln()
+    if(a == "SONGDO") a = "HIGHSCHOOL"
+    if(a == "CODE") a = "MASTER"
+     if(a == "2023") a = "0611"
+     if(a == "ALGORITHM") a = "CONTEST"
+    println(a)
+}
