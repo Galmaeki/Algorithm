0ee209a8edeb0e67a4fb7c3a1f2d8f398dea60e0
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2024-04-13 00:13:26 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 60 ms, Memory: 5328 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HELLOWORLD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1");
+            Console.WriteLine("0");
+        }
+    }
+}
