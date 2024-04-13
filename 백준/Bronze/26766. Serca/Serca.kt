178fe5a25404c3d0690a4bdd64c227ed3b481d73
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2024-04-13 11:24:01 +0900

    [Bronze V] Title: Serca, Time: 8388 ms, Memory: 98620 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.kt
@@ -0,0 +1,17 @@
+fun main(){
+    val count = readln().toInt()
+    for(i in 0 until count)
+        solution()
+}
+
+fun solution(){
+    println(" @@@   @@@ ")
+    println("@   @ @   @")
+    println("@    @    @")
+    println("@         @")
+    println(" @       @ ")
+    println("  @     @  ")
+    println("   @   @   ")
+    println("    @ @    ")
+    println("     @     ")
+}
