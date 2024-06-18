c39ebcec60b7f3f54794fe05237314d42cc8afb4
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2024-06-18 14:17:11 +0900

    [level 4] Title: 징검다리, Time: 20.27 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int distance, int[] rocks, int n) {
+        int answer = 0;
+
+        Arrays.sort(rocks);
+
+        int l = 1;
+        int r = distance;
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            int delete = 0;
+            int start = 0;
+
+            for (int i = 0; i < rocks.length; i++) {
+                if (rocks[i] - start < mid) {
+                    delete++;
+                } else {
+                    start = rocks[i];
+                }
+            }
+            
+                        if (distance - start < mid) {
+                delete++;
+            }
+
+            if (delete > n) {
+                r = mid-1;
+            } else {
+                answer = mid;
+                l = mid+1;
+            }
+
+        }
+
+        return answer;
+    }
+}
