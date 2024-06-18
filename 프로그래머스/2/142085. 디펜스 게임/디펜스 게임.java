efeeaa1e8b1b3a3cc950e818205f95977da89b2f
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2024-06-18 11:49:42 +0900

    [level 2] Title: 디펜스 게임, Time: 1.21 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+        int answer = 0;
+        PriorityQueue<Integer> pQ = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i : enemy) {
+            n -= i;
+            pQ.add(i);
+            if (n < 0) {
+                if (k > 0) {
+                    k--;
+                    n += pQ.poll();
+                } else {
+                    break;
+                }
+            }
+            answer++;
+        }
+
+        return answer;
+    }
+}
