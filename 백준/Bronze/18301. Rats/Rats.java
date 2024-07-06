1e0ced5bd872ffb98d32e2adfa3534af8526a752
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2024-07-06 21:47:01 +0900

    [Bronze V] Title: Rats, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/18301. Rats/Rats.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        System.out.println((a + 1) * (b + 1) / (c + 1) - 1);
+    }
+}
