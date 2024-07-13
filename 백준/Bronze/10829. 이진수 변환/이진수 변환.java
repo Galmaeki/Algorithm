f9282f95d6dcc010b2f61c0106a65b2e7f506a3b
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2024-07-13 22:33:13 +0900

    [Bronze II] Title: 이진수 변환, Time: 104 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Bronze/10829. 이진수 변환/이진수 변환.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(Long.toString(Long.parseLong(br.readLine()),2));
+    }
+}
