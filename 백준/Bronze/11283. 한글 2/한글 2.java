68a658d8beb47a0982b2cddbda6182d93fd9bb67
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2025-02-11 21:13:44 +0900

    [Bronze IV] Title: 한글 2, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        String a = br.readLine();
+
+        System.out.println(a.charAt(0)-44031);
+    }
+}
