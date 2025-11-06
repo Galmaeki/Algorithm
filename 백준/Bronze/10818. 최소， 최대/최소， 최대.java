dce07dc2d681afb63fabb304314a5218876f693e
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2025-11-06 17:15:31 +0900

    [Bronze III] Title: 최소, 최대, Time: 432 ms, Memory: 87676 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,18 +1,23 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        br.readLine();
-        int max = -1000000;
-        int min = 1000000;
-        String[] read = br.readLine().split(" ");
-        for (String a : read) {
-            if (max < Integer.parseInt(a))
-                max = Integer.parseInt(a);
-            if (min > Integer.parseInt(a))
-                min = Integer.parseInt(a);
+        int c = Integer.parseInt(br.readLine());
+        int max = -100000000, min = 100000000;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < c; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            if (max < a) max = a;
+            if (min > a) min = a;
         }
-        System.out.println(min +" " + max);
+        sb.append(min).append(" ").append(max);
+        System.out.println(sb);
     }
 }
