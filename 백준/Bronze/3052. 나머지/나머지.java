c0011f68b2e2990beba985d823a35b5c6e584215
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2025-11-12 17:50:48 +0900

    [Bronze II] Title: 나머지, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,13 +1,18 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+import java.util.TreeSet;
 
-public class Main{
-    public static void main(String[] args)throws IOException{
+public class Main {
+    public static void main(String[] args) throws IOException {
+        TreeSet<Integer> ts = new TreeSet<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashSet hs = new HashSet<Integer>();
-        for(int i =0;i<10;i++){
-            hs.add(Integer.parseInt(br.readLine())%42);
+
+        for (int i = 0; i < 10; i++) {
+            ts.add(Integer.parseInt(br.readLine())%42);
         }
-        System.out.println(hs.size());
+
+        System.out.println(ts.size());
     }
 }
